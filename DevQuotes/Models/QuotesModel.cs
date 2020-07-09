bf5739d4876ad242cc3359d33bdd4ece61ffec9e
Oluwagbenga Matthew Afolabi <gbenga@lubred.com>
2020-07-09 19:22:58 +0100

    Add project files.

---
+++ DevQuotes/Models/QuotesModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DevQuotes.Models
+{
+    public class QuotesModel
+    {
+        public string _id { get; set; }
+        public string id { get; set; }
+        public string sr { get; set; }
+        public string en { get; set; }
+        public string author { get; set; }
+        public string addedBy { get; set; }
+        public string source { get; set; }
+        public double rating { get; set; }
+        public int numberOfVotes { get; set; }
+    }
+}
---
+++ DevQuotes/Utilities/Helpers.cs
@@ -0,0 +1,11 @@
+﻿namespace DevQuotes.Utilities
+{
+    public static class Helpers
+    {
+        public static string Trunc(this string value, int count)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= count ? value : $"{value.Substring(0, count)}...";
+        }
+    }
+}
